4a36d277fa3c4109fdafcbc84670523c09a9fa02
hvullas <ullashv21@gmail.com>
2024-06-13 19:53:18 +0530

    configured helper, routers and controllers

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "jwt-auth-gin/controllers"
+	"jwt-auth-gin/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authentication())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
